a0c0d262f852e39b01224a94c5c4d0c356d0f6a7
Siddharth Verma <siddharthverma3904@gmail.com>
2023-01-19 23:45:47 +0530

    4x4 using enum

--- sudo.c
+++ sudo.c
@@ -1,19 +1,81 @@
 #include <stdio.h>
-#include <windows.h>
-#include <conio.h>
 
-#define for(i, n) for(int i=0; i<n; i++)
+#define fr(i, n) for(int i=0; i<n; i++)
 
+enum value{a=1, b=2, c=3, d=4, e=0};
 
+int chkcel(enum value sudo[4][4], int i, int j) {
+    int prob=0, k=0, pssbl[12];
+    int box=boxno(i, j);
+    fr(num, 4) {
+        //check row
+        fr(p, 4) {
+            if(p==j) continue;
+            if(sudo[i][p]==num+1) prob=1;
+        }
+        //check colm
+        fr(q, 4) {
+            if(q==i) continue;
+            if(sudo[q][j]==num+1) prob=1;
+        }
+        //check box
+        if(box==1) {
+            for(int a=0; a<2; a++) {
+                for(int b=0; b<2; b++) {
+                    if(a==i || b==j) continue;
+                    if(sudo[a][b]==num+1) prob=1;
+                }
+            }
+        }
+        if(box==2) {
+            for(int a=0; a<2; a++) {
+                for(int b=2; b<4; b++) {
+                    if(a==i || b==j) continue;
+                    if(sudo[a][b]==num+1) prob=1;
+                }
+            }
+        }
+        if(box==3) {
+            for(int a=2; a<4; a++) {
+                for(int b=0; b<2; b++) {
+                    if(a==i || b==j) continue;
+                    if(sudo[a][b]==num+1) prob=1;
+                }
+            }
+        }
+        if(box==4) {
+            for(int a=2; a<4; a++) {
+                for(int b=2; b<4; b++) {
+                    if(a==i || b==j) continue;
+                    if(sudo[a][b]==num+1) prob=1;
+                }
+            }
+        }
+        if(prob==0) {
+            pssbl[k]=num+1;
+            k++;
+        }
+        prob=0;
+    }
+    if(k==1) return pssbl[0];
+    else return 0;
+}
+
+int boxno(int i, int j) {
+    if(i<2 && j<2) return 1;
+    if(i<2 && j>1) return 2;
+    if(i>1 && j<2) return 3;
+    if(i>1 && j>1) return 4;
+}
 
 void main() {
-    enum value{a=1, b=2, c=3, d=4, e=0};
     enum value sudo[4][4];
-    int n;
+    int n, empty=0;
     printf("Enter Values: \n");
-    for(i, 4) {
-        for(j, 4) {
+    fr(i, 4) {
+        fr(j, 4) {
             scanf("%d", &n);
+            if(n==0) empty++;
             switch(n) {
                 case 1 :
                     sudo[i][j]=a;
@@ -33,14 +95,32 @@ void main() {
             }
         }
     }
-    printf("\nYou Entered: \n");
-    for(p, 4) {
-        for(q, 4) {
+    printf("\nYou entered: \n");
+    fr(p, 4) {
+        fr(q, 4) {
             if(sudo[p][q]==0) printf("* ");
             else printf("%d ", sudo[p][q]);
         }
         printf("\n");
     }
-    // if(n>0 && n<5) printf("%d", val);
-    // else printf("*");
+    while(empty>0) {
+        fr(a, 4) {
+            fr(b, 4) {
+                if(sudo[a][b]==0) {
+                    sudo[a][b]=chkcel(sudo, a, b);
+                    if(sudo[a][b]!=0) empty--;
+                }
+                if(empty==0) break;
+            }
+            if(empty==0) break;
+        }
+    }
+    printf("\nSolved: \n");
+    fr(p, 4) {
+        fr(q, 4) {
+            printf("%d ", sudo[p][q]);
+        }
+        printf("\n");
+    }
+    printf("\n%d\n", c);
 }
